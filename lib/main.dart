60f3f8efd81c42fb5defbf6175af064915de67a5
Juleles <jw.custodio@sarria.salesians.cat>
2023-10-16 19:43:39 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+// ignore_for_file: prefer_const_constructors
+// showBottomSheet
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Recuperar el valor d''un camp de text',
+      home: MyCustomForm(),
+    );
+  }
+}
+
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({super.key});
+  @override
+  State<MyCustomForm> createState() => _MyCustomFormState();
+}
+
+class _MyCustomFormState extends State<MyCustomForm> {
+  final myController = TextEditingController();
+
+  @override
+  void dispose() {
+    myController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Jules William Custodio 23 - 24'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: TextField(
+          controller: myController,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+              backgroundColor: Colors.transparent,
+              context: context,
+              builder: (BuildContext context) {
+                return Container(
+                    decoration: const BoxDecoration(
+                      color: Color.fromARGB(255, 238, 153, 97),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(25),
+                        topRight: Radius.circular(25),
+                      ),
+                    ),
+                    height: 200,
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Text(myController.text, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                          ElevatedButton(
+                            onPressed: () => Navigator.of(context).pop(),
+                            child: const Text('Close BottomSheet', style: TextStyle(fontSize: 14),),
+                          )
+                        ],
+                      ),
+                    ));
+              });
+        },
+        tooltip: 'Mostra el valor!',
+        child: const Icon(Icons.text_fields),
+      ),
+    );
+  }
+}
